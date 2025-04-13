8d5d3fc465d68af3ec20439a9f5c7693c0722037
F1N3X <quartzman92@gmail.com>
2025-04-13 23:14:37 +0200

    data validation

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce54222455da8403abe430d03fa519d2e0a06b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc23db0fc0834ebb8888e9b29750da6acb8fd63")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
