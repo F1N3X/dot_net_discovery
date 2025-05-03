48067bfe2d946a2a475ffa738e8b0f7d6dc6e32f
F1N3X <quartzman92@gmail.com>
2025-05-03 23:52:00 +0200

    login system and stock GET all Auth security

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1564106cdf18d9f25684465a8e3769ebf8121ed3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0f781696066ff41468143fb97157d613d9e5ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
